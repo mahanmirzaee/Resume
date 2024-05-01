9359d5d708b378ccb45c805c58c63be0869c9ac4
Mahan Mirzaie <mahanmirzaee11@gmail.com>
2024-05-01 20:57:48 +0330

    done

--- js/game.js
+++ js/game.js
@@ -1,14 +1,23 @@
+const navToggleIcon = document.querySelector(".nav__toggle-icon");
+const menu = document.querySelector(".menu");
+const cover = document.querySelector(".cover");
+const showresume = document.querySelectorAll(".resume-list__item");
 
-const navToggleIcon=document.querySelector(".nav__toggle-icon")
-const menu=document.querySelector(".menu")
-const cover=document.querySelector(".cover")
+const clickHandler = () => {
+  navToggleIcon.classList.toggle("nav__toggle-open");
+  menu.classList.toggle("menu--open");
+  cover.classList.toggle("cover--show");
+};
 
-const clickHandler=()=>{
+navToggleIcon.addEventListener("click", clickHandler);
 
-    navToggleIcon.classList.toggle("nav__toggle-open")
-    menu.classList.toggle("menu--open")
-    cover.classList.toggle("cover--show")
-}
+const showHandler = (event) => {
+  showresume.forEach((item) => {
+    item.classList.remove("resume-list__item--active");
+    event.target.classList.add("resume-list__item--active");
+  });
+};
 
-navToggleIcon.addEventListener("click",clickHandler)
-
+showresume.forEach((button) => {
+  button.addEventListener("click", showHandler);
+});
